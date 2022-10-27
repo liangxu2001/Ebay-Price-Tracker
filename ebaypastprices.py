06820097e64a7be991c3847e26cbc7757fe5dd24
liangxu96 <liangxu2001@gmail.com>
2022-10-26 23:12:32 -0400

    Updated to include features of whitelisting words

--- ebaypastprices.py
+++ ebaypastprices.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd 
 
-url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=iphone+11+64gb&_sacat=0&LH_TitleDesc=0&LH_Complete=1&LH_Sold=1&rt=nc&Storage%2520Capacity=64%2520GB&_dcat=9355'
+url = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=Sylveon+V+TG14+psa+10&_sacat=0&LH_TitleDesc=0&rt=nc&_odkw=Sylveon+V+TG14+psa&_osacat=0&LH_Complete=1&LH_Sold=1'
 
 #Create a function that will parse the URL and requests the data from page and makes soup variable so we can pocess. 
 def get_data(url):
@@ -44,6 +44,22 @@ def getAverage(list):
         sum += data["soldprice"]
     return sum / len(list)
 
+#Goes through the dictonary and removes any words from the title that we don't want
+def requiredWords(list, keywords):
+    newList = []
+    for listing in list:
+        title = listing['title']
+        for word in keywords:
+            if word in title:
+                newList.append(listing)
+    
+    return newList
+
+
+
 soup = get_data(url)
 list = parse(soup)
-print(getAverage(list))
+keywords = ['PSA 10'] 
+filteredList = requiredWords(list, keywords)
+print('Filtered:', getAverage(filteredList))
+print('No Filter:', getAverage(list))
